6ef40893e29972dafdbf981c7bcba99d1f581f0b
Fernando Henrique <fernandohjunqueira@protonmail.com>
2022-05-12 13:38:10 -0300

    add body to AnalisadorDeAprovacao
    Add body to class AnalisadorDeAprovacao according to instructions from
    MiniEP7.

--- src/main/kotlin/domain/AnalisadorDeAprovacao.kt
+++ src/main/kotlin/domain/AnalisadorDeAprovacao.kt
@@ -1,15 +1,19 @@
 package domain
 
+import domain.criterios.*
+
 class AnalisadorDeAprovacao {
+    private var criterio: CriterioDeAprovacao = Basico()
 
-    // ---------------------------------
-    //
-    //      Seu código deve vir aqui
-    //
-    //      Defina atributos e métodos conforme especificado
-    //      no arquivo de teste encontrado em
-    //      'src/test/kotlin/domain/AnalisadorDeAprovacaoTest'
-    //
-    // ---------------------------------
+    fun defineCriterio(novoCriterio: CriterioDeAprovacao) {
+        criterio = novoCriterio
+    }
 
+    fun fechaBoletim(boletim: Boletim) : BoletimFechado {
+        return BoletimFechado(
+            boletim.mediaEPs,
+            boletim.mediaMiniEPs,
+            criterio.mediaFinal(boletim),
+            criterio.estaAprovado(boletim))
+    }
 }
